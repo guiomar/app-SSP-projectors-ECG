5e22d54c1fdc39aa4806dcb8502b1b6f96059983
Maximilien Chaumon <maximilien.chaumon@icm-institute.org>
2023-04-19 14:46:10 +0200

    a few updates and improved saving figures

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ import mne
 import helper
 import numpy as np
 import pandas as pd
+
+#workaround for -- _tkinter.TclError: invalid command name ".!canvas"
+# so execution won't hang when figures are shown
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 
@@ -29,7 +34,7 @@ ecg_projs, ecg_events = mne.preprocessing.compute_proj_ecg(raw, raw_event=None, 
             n_jobs=-1, ch_name=config['ch_name'], reject=None, flat=None, bads=[],
             avg_ref=config['avg_ref'], no_proj=config['no_proj'], event_id=config['event_id'], ecg_l_freq=config['ecg_l_freq'], ecg_h_freq=config['ecg_h_freq'],
             tstart=config['tstart'],
-            qrs_threshold=config['qrs_threshold'], filter_method=config['filter_method'], iir_params=config['iir_params'], copy=False, return_drop_log=False,
+            qrs_threshold=config['qrs_threshold'], filter_method=config['filter_method'], iir_params=config['iir_params'], copy=True, return_drop_log=False,
             meg=config['meg'])
 
 mne.write_proj('out_dir/proj.fif', ecg_projs, overwrite=True)
@@ -41,15 +46,8 @@ fig_ep.savefig(os.path.join('out_figs','ecg_projectors.png'))
 ecg_evoked = mne.preprocessing.create_ecg_epochs(raw).average()
 ecg_evoked.apply_baseline((None, None))
 
-e=ecg_evoked.plot_joint(picks='mag')
-e.savefig(os.path.join('out_figs','meg.png'))
-
-e=ecg_evoked.plot_joint(picks='grad')
-e.savefig(os.path.join('out_figs','grad.png'))
-
-e=ecg_evoked.plot_joint(picks='eeg')
-e.savefig(os.path.join('out_figs','eeg.png'))
-
+f = ecg_evoked.plot_joint()
+[fig.savefig(os.path.join('out_figs',f'ecg_{i}.png')) for i, fig in enumerate(f)]
 
 report = mne.Report(title='SSP ECG Projectors')
 report.add_projs(info=raw.info, projs=ecg_projs, title = 'SSP ECG Projectors')
