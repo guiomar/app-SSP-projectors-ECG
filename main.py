ce7ee4d31b14cb4fbb115e88dd415c11c2fc11d4
Maximilien Chaumon <maximilien.chaumon@icm-institute.org>
2023-04-19 13:54:06 +0200

    cleanup parameters and add report

--- main.py
+++ main.py
@@ -23,38 +23,38 @@ with open('config.json','r') as config_f:
 fname = config['mne']
 raw = mne.io.read_raw_fif(fname, verbose=False)
 
-# parameters:
-# raw_event is unused
 ecg_projs, ecg_events = mne.preprocessing.compute_proj_ecg(raw, raw_event=None, tmin=config['tmin'], tmax=config['tmax'], n_grad=config['n_grad'],
-            n_mag=config['n_mag'], n_eeg=config['n_eeg'], l_freq=config['l_freq'], h_freq=config['h_freq'], average=config['average'], filter_length=config['filter_length'], 
+            n_mag=config['n_mag'], n_eeg=config['n_eeg'], l_freq=config['l_freq'], h_freq=config['h_freq'], average=config['average'], 
+            filter_length=config['filter_length'], 
             n_jobs=-1, ch_name=config['ch_name'], reject=None, flat=None, bads=[],
-            avg_ref=config['avg_ref'], no_proj=config['no_proj'], event_id=config['event_id'], ecg_l_freq=config['ecg_l_freq'], ecg_h_freq=config['ecg_h_freq'], tstart=config['tstart'],
-            qrs_threshold=config['qrs_threshold'], filter_method=config['filter_method'], iir_params=None, copy=False, return_drop_log=True, meg=config['meg'])
+            avg_ref=config['avg_ref'], no_proj=config['no_proj'], event_id=config['event_id'], ecg_l_freq=config['ecg_l_freq'], ecg_h_freq=config['ecg_h_freq'],
+            tstart=config['tstart'],
+            qrs_threshold=config['qrs_threshold'], filter_method=config['filter_method'], iir_params=config['iir_params'], copy=False, return_drop_log=False,
+            meg=config['meg'])
 
 mne.write_proj('out_dir/proj.fif', ecg_projs, overwrite=True)
 
-ecg_projs = ecg_projs[3:]
-
 # == FIGURES ==
-plt.figure(1)
 fig_ep = mne.viz.plot_projs_topomap(ecg_projs, info=raw.info)
 fig_ep.savefig(os.path.join('out_figs','ecg_projectors.png'))
 
 ecg_evoked = mne.preprocessing.create_ecg_epochs(raw).average()
 ecg_evoked.apply_baseline((None, None))
 
-plt.figure(2)
 e=ecg_evoked.plot_joint(picks='mag')
 e.savefig(os.path.join('out_figs','meg.png'))
 
-plt.figure(3)
 e=ecg_evoked.plot_joint(picks='grad')
 e.savefig(os.path.join('out_figs','grad.png'))
 
-plt.figure(4)
 e=ecg_evoked.plot_joint(picks='eeg')
 e.savefig(os.path.join('out_figs','eeg.png'))
 
+
+report = mne.Report(title='SSP ECG Projectors')
+report.add_projs(info=raw.info, projs=ecg_projs, title = 'SSP ECG Projectors')
+
+report.save('out_report/report_ssp.html', overwrite=True)
 
 
 
